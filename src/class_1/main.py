911ed5168c28f2e19e52fb2186096ee6b388c8ff
Faraz Ahmed <farazahmediu01@gmail.com>
2025-04-26 12:21:38 +0500

    refactor: clean up code structure and improve readability

--- src/class_1/main.py
+++ src/class_1/main.py
@@ -7,20 +7,17 @@ import os
 dotenv.load_dotenv()
 GEMENI_API_KEY = os.getenv("GEMENI_API_KEY")
 
-client = OpenAI(api_key=GEMENI_API_KEY,
-     base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+client = OpenAI(
+    api_key=GEMENI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
 )
 
 response = client.chat.completions.create(
     model="gemini-2.0-flash",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
-        
-        {
-            "role": "user",
-            "content": "What is the capital of France?"
-        }
-    ]
+        {"role": "user", "content": "What is the capital of Pakistan?"},
+    ],
 )
 
 response = response.choices[0].message
