2494853745428e04b427d219af7403a7108dfd88
Faraz Ahmed <farazahmediu01@gmail.com>
2025-06-26 02:03:52 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
-def main():
-    print("Hello from genai!")
+from openai import OpenAI
+import dotenv
+import rich
+import os
 
 
-if __name__ == "__main__":
-    main()
+dotenv.load_dotenv()
+GEMENI_API_KEY = os.getenv("GEMENI_API_KEY")
+
+client = OpenAI(
+    api_key=GEMENI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+response = client.chat.completions.create(
+    model="gemini-2.0-flash",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "What is the capital of Pakistan?"},
+    ],
+)
+
+response = response.choices[0].message
+rich.print(response)
